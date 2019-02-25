0e9af7035991c37cb7624f290beb22c7fb9793d1
Adrian Pintilie <adrianpintilie96@gmail.com>
2019-02-25 22:07:57 +0200

    requirments added

--- sentenceToEmojiService/sentenceToEmojiService.py
+++ sentenceToEmojiService/sentenceToEmojiService.py
@@ -1,11 +1,11 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
-from resources.sentenceToEmoji import SentenceToEmoji
+from resources import sentenceToEmojiRoute as route
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(SentenceToEmoji, '/analyze/<string:input_text>')
+api.add_resource(route.SentenceToEmoji, '/analyze/<string:input_text>')
 
 if __name__ == '__main__':
     app.run()
